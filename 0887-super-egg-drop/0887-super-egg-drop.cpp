cc39aaa0c75c21c9fb61f5e2eaed6539249847f8
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-02 14:29:57 +0530

    Time: 3 ms (69.41%), Space: 7.9 MB (99.02%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int findTotalMoves(int eggs, int moves, vector<vector<int>>& dp) {
+    int findTotalMoves(int eggs, int moves) {
         if(eggs == 0 || moves == 0)
         return 0;
 
         if(eggs == 1)
         return moves;
 
-        if(dp[eggs][moves] != -1)
-        return dp[eggs][moves];
-
-        return dp[eggs][moves] = 1+findTotalMoves(eggs-1, moves-1, dp)+findTotalMoves(eggs, moves-1, dp);
+        return 1+findTotalMoves(eggs-1, moves-1)+findTotalMoves(eggs, moves-1);
     }
     int superEggDrop(int k, int n) {
         int moves = 0;
-        vector<vector<int>> dp(k+1, vector<int>(n+1, -1));
-
-        while(findTotalMoves(k, moves, dp)<n){
+        while(findTotalMoves(k, moves)<n){
             moves++;
         }
         return moves;
