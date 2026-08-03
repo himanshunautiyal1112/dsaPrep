281c2ec28d1f5df18a47278a295adaa6ca0cd7c5
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-03 20:14:51 +0530

    Time: 64 ms (45.06%), Space: 33.1 MB (35.28%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     static bool com(string s1, string s2) {
-        return s1.size()<s2.size();
+        return s1.size() < s2.size();
     }
-    bool isPredecessor(string word1, string word2) {
-        if(word1.size()+1 != word2.size())
-        return 0;
-
-        int i = word1.size(), j = word2.size();
-
-        while(i && j) {
-            if(word1[i-1] == word2[j-1])
-            i--, j--;
-            else 
-            j--;
-        }
-
-        return i==0? 1: 0;
-    }
+    
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), com);
-        int n = words.size();
-        vector<int> LSC(n, 1);
-
+        unordered_map<string, int> dp;
         int maxLen = 1;
 
-        for(int i=1; i<n; i++) {
-            for(int j=0; j<i; j++) {
-                if(isPredecessor(words[j], words[i])) {
-                    LSC[i] = max(LSC[i], 1+LSC[j]);
+        for(auto w: words) {
+            dp[w] = 1;
+            for(int i=0; i<w.size(); i++) {
+                string s = w.substr(0, i) + w.substr(i+1);
+
+                if(dp.count(s)) {
+                    dp[w] = max(dp[w], 1+dp[s]);
                 }
             }
-            maxLen = max(maxLen, LSC[i]);
+            maxLen = max(maxLen, dp[w]);
         }
 
         return maxLen;
