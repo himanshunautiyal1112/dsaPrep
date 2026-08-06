5f73af276b2c1f351b5b3c626b2afd6f76c7d79c
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-06 20:31:35 +0530

    Time: 0 ms (100%), Space: 11.9 MB (32.3%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int totalPathWithObt(int i, int j, int row, int col, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp) {
-        if(i == row-1 && j==col-1)
-        return 1;
-
-        if(i>=row || j>=col)
+        if(i >= row || j >= col)
         return 0;
 
         if(obstacleGrid[i][j] == 1)
         return 0;
+
+        if(i == row-1 && j == col-1)
+        return 1;
 
         if(dp[i][j] != -1)
         return dp[i][j];
@@ -22,8 +22,27 @@ public:
         if(obstacleGrid[n-1][m-1] == 1)
         return 0;
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        if(obstacleGrid[0][0] == 1)
+        return 0;
 
-        return totalPathWithObt(0, 0, n, m, obstacleGrid, dp);
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+        dp[n-1][m-1] = 1;
+        
+        for(int i = n-1; i >= 0; i--) {
+
+            for(int j = m-1; j >= 0; j--) {
+
+                if(obstacleGrid[i][j] == 1)
+                dp[i][j] = 0;
+
+                else if(i == n-1 && j == m-1)
+                dp[i][j] = 1;
+
+                else
+                dp[i][j] = dp[i][j+1] + dp[i+1][j];
+            }
+        }
+        return dp[0][0];
+        //return totalPathWithObt(0, 0, n, m, obstacleGrid, dp);
     }
 };
