d689485b798edaebf679444783ee1b2b939765a6
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-06 19:44:34 +0530

    Time: 0 ms (100%), Space: 9.3 MB (69.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,14 @@ public:
         return dp[i][j] = totoalPath(i, j+1, m, n, dp) + totoalPath(i+1, j, m, n, dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return totoalPath(0,0, m, n, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 1));
+
+        for(int i=m-2; i>=0; i--) {
+            for(int j=n-2; j>=0; j--) {
+                dp[i][j] = dp[i][j+1] + dp[i+1][j];
+            }
+        }
+        return dp[0][0];
+        //return totoalPath(0,0, m, n, dp);
     }
 };
