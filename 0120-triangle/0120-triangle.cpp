6e0978d8c824efd0fb0a258dc6759489924b48f8
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-07 13:18:44 +0530

    Time: 0 ms (100%), Space: 13.3 MB (59.69%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumSumPath(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+        if(i == n-1)
+        return triangle[i][j];
+
+        if(dp[i][j] != -1)
+        return dp[i][j];
+
+        return dp[i][j] = triangle[i][j] + min(minimumSumPath(i+1, j, n, triangle, dp), minimumSumPath(i+1, j+1, n, triangle, dp));
+
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i=0; i<n; i++)
+        dp[n-1][i] = triangle[n-1][i];
+        
+        for(int i=n-2; i>=0; i--) {
+            for(int j=i; j>=0; j--) {
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+        //return minimumSumPath(0, 0, n, triangle, dp);
+    }
+};
