9dada91fa86deb1132c9b0d78550ec0d4c47d914
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-08 12:50:15 +0530

    Time: 0 ms (100%), Space: 14.2 MB (97.06%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -20,25 +20,43 @@ public:
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, INT_MAX));
-
-        for(int j=0; j<n; j++)
-        dp[n-1][j] = matrix[n-1][j];
 
         for(int i=n-2; i>=0; i--) {
             for(int j=n-1; j>=0; j--) {
-                if(j==0)
-                dp[i][j] = matrix[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
-                else if(j==n-1)
-                dp[i][j] = matrix[i][j] + min(dp[i+1][j-1], dp[i+1][j]);
-                else 
-                dp[i][j] = matrix[i][j] + min({dp[i+1][j-1], dp[i+1][j], dp[i+1][j+1]});
+                int minChoice = matrix[i+1][j];
+                if(j>0) minChoice = min(minChoice, matrix[i+1][j-1]);
+                if(j<n-1) minChoice = min(minChoice, matrix[i+1][j+1]);
+
+                matrix[i][j] += minChoice;
             }
         }
         int result = INT_MAX;
+        
         for(int i=0; i<n; i++) {
-            result = min(result, dp[0][i]);
+            result = min(result, matrix[0][i]);
         }
         return result;
+        //dp
+
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, INT_MAX));
+
+        // for(int j=0; j<n; j++)
+        // dp[n-1][j] = matrix[n-1][j];
+
+        // for(int i=n-2; i>=0; i--) {
+        //     for(int j=n-1; j>=0; j--) {
+        //         if(j==0)
+        //         dp[i][j] = matrix[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+        //         else if(j==n-1)
+        //         dp[i][j] = matrix[i][j] + min(dp[i+1][j-1], dp[i+1][j]);
+        //         else 
+        //         dp[i][j] = matrix[i][j] + min({dp[i+1][j-1], dp[i+1][j], dp[i+1][j+1]});
+        //     }
+        // }
+        // int result = INT_MAX;
+        // for(int i=0; i<n; i++) {
+        //     result = min(result, dp[0][i]);
+        // }
+        // return result;
     }
 };
