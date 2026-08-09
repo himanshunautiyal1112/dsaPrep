75f73688d0bef00f454f0ac10abd4f87111114fa
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-09 15:06:25 +0530

    Time: 27 ms (91.88%), Space: 28 MB (60.49%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -36,9 +36,48 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(n+1, -1)));
-        int ans = maxCherry(0, 0, 0,n, grid, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(n+1, INT_MIN)));
+        dp[n-1][n-1][n-1] = grid[n-1][n-1];
 
-        return max(0, ans);
+        for(int r1=n-1; r1>=0; r1--) {
+            for(int c1=n-1; c1>=0; c1--) {
+                for(int r2=n-1; r2>=0; r2--) {
+                    if(r1 == n-1 && c1 == n-1)
+                    continue;
+
+                    int c2 = (r1+c1)-r2;
+
+                    if(c2 < 0 || c2 >= n)
+                    continue;
+
+
+                    if(grid[r1][c1] == -1 || grid[r2][c2] == -1)
+                    continue;
+
+                    int cherry = grid[r1][c1];
+
+                    if(r1!=r2 || c1!=c2)
+                    cherry+=grid[r2][c2];
+
+                    int DD = dp[r1+1][c1][r2+1];
+                    int DR = dp[r1+1][c1][r2]; 
+                    int RD = dp[r1][c1+1][r2+1]; 
+                    int RR = dp[r1][c1+1][r2]; 
+
+                    int pick = max({DD, DR, RD, RR});
+
+                    if(pick == INT_MIN)
+                    continue;
+
+                    dp[r1][c1][r2] = cherry+pick;
+
+                }
+            }
+        }
+
+        return max(0, dp[0][0][0]);
+        // int ans = maxCherry(0, 0, 0,n, grid, dp);
+
+        // return max(0, ans);
     }
 };
