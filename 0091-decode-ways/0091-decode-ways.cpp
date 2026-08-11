74357cff9b4e752f50b61e46ea4b2df11ca445f2
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-11 16:50:48 +0530

    Time: 0 ms (100%), Space: 8.8 MB (46.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -23,7 +23,26 @@ public:
     }
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n+1, -1);
-        return totalWays(0, n, s, dp);
+        vector<int> dp(n+1, 0);
+        if(s[0] == '0')
+        return 0;
+
+        dp[n] = 1;
+        for(int i=n-1; i>=0; i--) {
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+
+            int ways = dp[i+1];
+
+            if(i+1 < n) {
+                if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
+                ways += dp[i+2];
+            }
+            dp[i] = ways;
+        }
+        return dp[0];
+        //return totalWays(0, n, s, dp);
     }
 };
