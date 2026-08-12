1c4289d43f7586583e287f978860ed88baa0afe7
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-08-12 12:50:43 +0530

    Time: 0 ms (100%), Space: 16.1 MB (59.48%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxiMumPoints(int n, vector<int> &points, vector<int>& dp) {
+        if(n == 0)
+        return points[0];
+
+        if(n==1)
+        return max(points[0], points[1]);
+
+        if(dp[n] != -1)
+        return dp[n];
+
+        return dp[n] = max(points[n] + maxiMumPoints(n-2, points, dp), maxiMumPoints(n-1, points, dp));
+
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0)
+        return 0;
+
+        int maxi = *max_element(nums.begin(), nums.end());
+
+        vector<int> points(maxi+1,0);
+        vector<int> dp(maxi+1, -1);
+
+        for(int num: nums) {
+            points[num] += num;
+        }
+
+        return maxiMumPoints(*max_element(nums.begin(), nums.end()), points, dp); 
+    }
+};
