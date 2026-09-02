287017c5374b7815f9afe8ff829abb9d7216af73
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-09-02 14:39:09 +0530

    Time: 0 ms (100%), Space: 10.8 MB (89.41%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -21,8 +21,28 @@ public:
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
+        //inor(root, ans);
 
-        inor(root, ans);
+         while(root) {
+            if(!root->left) {
+                ans.push_back(root->val);
+                root = root->right;
+            } else {
+                TreeNode* temp = root->left;
+                
+                while(temp->right && temp->right != root)
+                temp = temp->right;
+                
+                if(!temp->right) {
+                    temp->right = root;
+                    root = root->left;
+                } else {
+                    temp->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+            }
+        }
 
         return ans;
     }
