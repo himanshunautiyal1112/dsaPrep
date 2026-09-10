08bef390f441de1be57d6bb848bff4b25003c76b
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-09-10 14:11:01 +0530

    Time: 0 ms (100%), Space: 60.8 MB (51.51%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,39 +11,67 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& arr) {
-        if(!root)
-        return;
+    //BASIC OR BRUTE FORCE T.C(O(N)) S.C[O(N)]
+    // void inorder(TreeNode* root, vector<int>& arr) {
+    //     if(!root)
+    //     return;
 
-        inorder(root->left, arr);
-        arr.push_back(root->val);
-        inorder(root->right, arr);
-    }
-    void consrtuct(TreeNode* root,  vector<int>& arr, int &index) {
-        if(!root)
-        return;
+    //     inorder(root->left, arr);
+    //     arr.push_back(root->val);
+    //     inorder(root->right, arr);
+    // }
+    // void consrtuct(TreeNode* root,  vector<int>& arr, int &index) {
+    //     if(!root)
+    //     return;
 
-        consrtuct(root->left, arr, index);
-        root->val = arr[index++];
-        consrtuct(root->right, arr, index);
-    }
+    //     consrtuct(root->left, arr, index);
+    //     root->val = arr[index++];
+    //     consrtuct(root->right, arr, index);
+    // }
+
+    //OPTIMAL METHOD [ USING MORRIS TRAVERSAL] TC[O(N)] S.C[O(1)];
     void recoverTree(TreeNode* root) {
-        vector<int> arr;
-        inorder(root, arr);
-        int first = -1, second = -1;
+        TreeNode* curr = NULL, *prev = NULL, *first = NULL, *second = NULL;
 
-        for(int i=1; i<arr.size(); i++) {
-            if(arr[i] < arr[i-1]) {
-                if(first == -1) {
-                    first = i-1;
-                    second = i;
+        while(root) {
+            if(!root->left) {
+                prev = curr;
+                curr = root;
+
+                if(prev && prev->val > curr->val) {
+                    if(!first)
+                    first = prev;
+
+                    second = curr;
                 }
-                second = i;
+
+                root = root->right;
+            } else {
+                TreeNode* temp = root->left;
+
+                while(temp->right && temp->right != root)
+                temp = temp->right;
+
+                if(!temp->right) {
+                    temp->right = root;
+                    root = root->left;
+                } else {
+                    temp->right = NULL;
+                    prev = curr;
+                    curr = root;
+
+                    if(prev && prev->val > curr->val) {
+                        if(!first)
+                        first = prev;
+
+                        second = curr;
+                    }
+
+                    root = root->right;
+                }
             }
         }
 
-        swap(arr[first], arr[second]);
-        int index = 0;
-        consrtuct(root, arr, index);
+        swap(first->val, second->val);
     }
 };
