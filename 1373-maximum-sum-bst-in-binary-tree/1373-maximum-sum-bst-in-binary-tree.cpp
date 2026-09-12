ab232fd246200d5c8d67dc20e9aa5203743cd7a2
himanshu <157032579+himanshunautiyal1112@users.noreply.github.com>
2026-09-12 13:40:19 +0530

    Time: 88 ms (37.73%), Space: 162 MB (20.52%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -12,66 +12,38 @@
  class Box {
     public:
         bool BST;
-        int size;
+        int sum;
         int minValue, maxValue;
         
-        Box(int data) {
+        Box() {
             BST = 1;
-            size = data;
-            minValue = data;
-            maxValue = data;
+            sum = 0;
+            minValue = INT_MAX;
+            maxValue = INT_MIN;
         }
 };
 class Solution {
 public:
     Box* find(TreeNode* root, int &maxSum) {
-        if(!root->left && !root->right) {
-            maxSum = max(maxSum, root->val);
-            return new Box(root->val);
+        if(!root) {
+            return new Box();
         }
-        else if(!root->left && root->right) {
-            Box* head = find(root->right, maxSum);
-            
-            if(head->BST && head->minValue > root->val) {
-                head->size+=root->val;
-                head->minValue = root->val;
-                maxSum = max(maxSum, head->size);
-                return head;
-            } else {
-                head->BST = 0;
-                return head;
-            }
-        }
-        else if(root->left && !root->right) {
-            Box* head = find(root->left, maxSum);
-            
-            if(head->BST && head->maxValue < root->val) {
-                head->size+=root->val;
-                head->maxValue = root->val;
-                maxSum = max(maxSum, head->size);
-                return head;
-            } else {
-                head->BST = 0;
-                return head;
-            }
+
+        Box* left = find(root->left, maxSum);
+        Box* right = find(root->right, maxSum);
+
+        if(left->BST && right->BST && left->maxValue < root->val && right->minValue > root->val) {
+            Box *head = new Box();
+
+            head->sum = root->val + left->sum + right->sum;
+            head->minValue = min(root->val, left->minValue);
+            head->maxValue = max(root->val, right->maxValue);
+
+            maxSum = max(maxSum, head->sum);
+            return head;
         } else {
-            Box* left = find(root->left, maxSum);
-            Box* right = find(root->right, maxSum);
-            
-            if(left->BST && right->BST && left->maxValue < root->val && right->minValue > root->val) {
-                Box* head = new Box(root->val);
-                
-                head->size += left->size + right->size;
-                head->minValue = left->minValue;
-                head->maxValue = right->maxValue;
-                
-                maxSum = max(maxSum, head->size);
-                
-                return head;
-            } else {
-                left->BST = 0;
-                return left;
-            }
+            left->BST = 0;
+            return left;
         }
     }
     int maxSumBST(TreeNode* root) {
